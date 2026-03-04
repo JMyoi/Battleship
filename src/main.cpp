fb76c2cbf7b190577202887cad159b92f62fd654
Jay Chen <jaychenjjc@gmail.com>
2026-03-04 01:23:16 -0500

    explosion animation works on hit

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Battleship");
 
     SetTargetFPS(60);
-    //Game game;
+    Game game;
 
-    Texture2D Explosion = LoadTexture("src/assets/Explosion.png");
-    Vector2 position = {0,0};
-    Rectangle frameRec = {0, 0, static_cast<float>(Explosion.width/7)-12, static_cast<float>(Explosion.height)};//-20 because it's leaking a bit to the left
-    int currentFrame = 0;
-    int frameCounter = 0;
-    int framesSpeed = 8;
-    bool active = false;
+    // Texture2D Explosion = LoadTexture("src/assets/Explosion.png");
+    // Vector2 position = {0,0};
+    // Rectangle frameRec = {0, 0, static_cast<float>(Explosion.width/7)-12, static_cast<float>(Explosion.height)};//-20 because it's leaking a bit to the left
+    // int currentFrame = 0;
+    // int frameCounter = 0;
+    // int framesSpeed = 8;
+    // bool active = false;
 
    
 
@@ -27,72 +27,72 @@ int main(void)
     while (!WindowShouldClose())
     {
         //1, Event handling and update
-       if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !active)
-        {
-            position = GetMousePosition();
-            active = true;
+    //    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !active)
+    //     {
+    //         position = GetMousePosition();
+    //         active = true;
 
-            // center using ONE frame size (not full spritesheet size)
-            position.x -= frameRec.width / 2.0f;
-            position.y -= frameRec.height / 2.0f;
+    //         // center using ONE frame size (not full spritesheet size)
+    //         position.x -= frameRec.width / 2.0f;
+    //         position.y -= frameRec.height / 2.0f;
 
-            currentFrame  = 0;
-            frameCounter = 0;
-            // active = true;   // redundant, can remove
-        }
-        if(active){
-            frameCounter++;
-            if(frameCounter >=(60/framesSpeed)){
-                frameCounter = 0;
-                currentFrame++;
+    //         currentFrame  = 0;
+    //         frameCounter = 0;
+    //         // active = true;   // redundant, can remove
+    //     }
+    //     if(active){
+    //         frameCounter++;
+    //         if(frameCounter >=(60/framesSpeed)){
+    //             frameCounter = 0;
+    //             currentFrame++;
     
-                if(currentFrame>6) {
-                    currentFrame= 0;
-                    active = false;
-                }
+    //             if(currentFrame>6) {
+    //                 currentFrame= 0;
+    //                 active = false;
+    //             }
     
-                frameRec.x = (static_cast<float>(currentFrame)) * (static_cast<float>(Explosion.width/7)-12);
-            }
+    //             frameRec.x = (static_cast<float>(currentFrame)) * (static_cast<float>(Explosion.width/7)-12);
+    //         }
 
-        }
+    //     }
 
         //2. Draw
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            if(active)
-                DrawTextureRec(Explosion, frameRec, position, WHITE);
+            // if(active)
+            //     DrawTextureRec(Explosion, frameRec, position, WHITE);
     
 
-            // switch(GameState state = game.getGameState()){
+            switch(GameState state = game.getGameState()){
 
-            //     case GameState::Menu:
-            //         game.drawMenu();
-            //         break;
-            //     case GameState::SetupP1:
-            //         game.drawP1Setup();
-            //         break;
-            //     case GameState::P2SetupTransition:
-            //         game.drawP2SetupTransition();
-            //         break;
-            //     case GameState::SetupP2:
-            //         game.drawP2Setup();
-            //         break;
-            //     case GameState::P1Transition:
-            //         game.drawP1Transition();
-            //         break;
-            //     case GameState::P2Transition:
-            //         game.drawP2Transition();
-            //         break;
-            //     case GameState::TurnP1:
-            //         game.drawP1Turn();
-            //         break;
-            //     case GameState::TurnP2:
-            //         game.drawP2Turn();
-            //         break;
-            //     case GameState::GameOver:
-            //         game.drawGameOver();
-            //         break;
-            // }
+                case GameState::Menu:
+                    game.drawMenu();
+                    break;
+                case GameState::SetupP1:
+                    game.drawP1Setup();
+                    break;
+                case GameState::P2SetupTransition:
+                    game.drawP2SetupTransition();
+                    break;
+                case GameState::SetupP2:
+                    game.drawP2Setup();
+                    break;
+                case GameState::P1Transition:
+                    game.drawP1Transition();
+                    break;
+                case GameState::P2Transition:
+                    game.drawP2Transition();
+                    break;
+                case GameState::TurnP1:
+                    game.drawP1Turn();
+                    break;
+                case GameState::TurnP2:
+                    game.drawP2Turn();
+                    break;
+                case GameState::GameOver:
+                    game.drawGameOver();
+                    break;
+            }
 
 
         EndDrawing();
