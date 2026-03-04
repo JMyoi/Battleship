181aba03a7f7b5053fc266b40728e78039e5ed31
Jay <127246162+JMyoi@users.noreply.github.com>
2026-03-04 16:55:49 -0500

    implementing background funciton

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+
 int main(void)
 {
     const int screenWidth = 1150;
@@ -17,7 +18,7 @@ int main(void)
 
     SetTargetFPS(60);
     Game game;
-    
+
     //background parallax image loading
     Texture2D background = LoadTexture("src/assets/Clouds/Clouds 1/1.png");
     Texture2D midgroundOne = LoadTexture("src/assets/Clouds/Clouds 1/2.png");
@@ -29,22 +30,28 @@ int main(void)
     float scrollingMidTwo = 0.0f;
     float scrollingFore = 0.0f;
     
+    //to scale teh image to fit the height of the screen
+    const float layerScale = (float)screenHeight / background.height;
+    const float backW = background.width * layerScale;
+    const float mid1W = midgroundOne.width * layerScale;
+    const float mid2W = midgroundTwo.width * layerScale;
+    const float foreW = foreground.width * layerScale;
+
     while (!WindowShouldClose())
     {
         UpdateMusicStream(BGM);
 
-        // Update
-        //----------------------------------------------------------------------------------
+
         scrollingBack -= 0.1f;
         scrollingMidOne -= 0.3f;
         scrollingMidTwo -= 0.5f;
         scrollingFore -= 1.0f;
 
         // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
-        if (scrollingBack <= -background.width*2) scrollingBack = 0;
-        if (scrollingMidOne <= -midgroundOne.width*2) scrollingMidOne = 0;
-        if (scrollingMidTwo <= -midgroundTwo.width*2) scrollingMidTwo = 0;
-        if (scrollingFore <= -foreground.width*2) scrollingFore = 0;
+        if (scrollingBack <= -backW) scrollingBack = 0;
+        if (scrollingMidOne <= -mid1W) scrollingMidOne = 0;
+        if (scrollingMidTwo <= -mid2W) scrollingMidTwo = 0;
+        if (scrollingFore <= -foreW) scrollingFore = 0;
 
 
         //2. Draw
@@ -52,20 +59,20 @@ int main(void)
             ClearBackground(RAYWHITE);
             // Draw background image twice
             // NOTE: Texture is scaled twice its size
-            DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ scrollingBack, 0 }, 0.0f, layerScale, WHITE);
+            DrawTextureEx(background, (Vector2){ backW + scrollingBack, 0 }, 0.0f, layerScale, WHITE);
 
             // Draw first midground image twice
-            DrawTextureEx(midgroundOne, (Vector2){ scrollingMidOne, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(midgroundOne, (Vector2){ midgroundOne.width*2 + scrollingMidOne, 20 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(midgroundOne, (Vector2){ scrollingMidOne, 0 }, 0.0f, layerScale, WHITE);
+            DrawTextureEx(midgroundOne, (Vector2){ mid1W + scrollingMidOne, 0 }, 0.0f, layerScale, WHITE);
 
             // Draw Second midground image twice
-            DrawTextureEx(midgroundOne, (Vector2){ scrollingMidTwo, 20 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(midgroundOne, (Vector2){ midgroundTwo.width*2 + scrollingMidTwo, 20 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(midgroundTwo, (Vector2){ scrollingMidTwo, 0 }, 0.0f, layerScale, WHITE);
+            DrawTextureEx(midgroundTwo, (Vector2){ mid2W + scrollingMidTwo, 0 }, 0.0f, layerScale, WHITE);
 
             // Draw foreground image twice
-            DrawTextureEx(foreground, (Vector2){ scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(foreground, (Vector2){ foreground.width*2 + scrollingFore, 70 }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(foreground, (Vector2){ scrollingFore, 0 }, 0.0f, layerScale, WHITE);
+            DrawTextureEx(foreground, (Vector2){ foreW + scrollingFore, 0 }, 0.0f, layerScale, WHITE);
     
             switch(GameState state = game.getGameState()){
                 case GameState::Menu:
