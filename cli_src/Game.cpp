2acf1230b40497f60915e393554536581ba5ca60
liuzx1557 <liuzx215@gmail.com>
2026-03-11 13:01:39 -0400

    Integrate AI turns into Game controller

--- cli_src/Game.cpp
+++ cli_src/Game.cpp
@@ -1,12 +1,16 @@
 #include "Game.h"
 
-Game::Game(int shipCount)
+Game::Game(int shipCount,
+           PlayerType p1Type,
+           PlayerType p2Type,
+           AIDifficulty p1Difficulty,
+           AIDifficulty p2Difficulty)
     : m_shipCount(shipCount),
       m_state(GameState::SetupP1),
       m_nextShipToPlace(0),
       m_winner(-1),
-      m_p1(shipCount),
-      m_p2(shipCount)
+      m_p1(shipCount, p1Type, p1Difficulty),
+      m_p2(shipCount, p2Type, p2Difficulty)
 {
     if (m_shipCount < 1) m_shipCount = 1;
     if (m_shipCount > 5) m_shipCount = 5;
@@ -19,6 +23,7 @@ int Game::currentPlayerIndex() const {
     if (m_state == GameState::SetupP2 || m_state == GameState::TurnP2) return 1;
     return -1;
 }
+
 int Game::winnerIndex() const { return m_winner; }
 
 Player& Game::player(int idx) { return (idx == 0) ? m_p1 : m_p2; }
@@ -29,7 +34,7 @@ bool Game::placeNextShip(Position start, Direction dir) {
 
     Player& cur = (m_state == GameState::SetupP1) ? m_p1 : m_p2;
     int shipIndex = m_nextShipToPlace;
-    int shipSize  = shipSizeForIndex(shipIndex);
+    int shipSize = shipSizeForIndex(shipIndex);
 
     bool ok = cur.board().placeShip(shipIndex, shipSize, start, dir, cur.ships());
     if (!ok) return false;
@@ -38,14 +43,16 @@ bool Game::placeNextShip(Position start, Direction dir) {
     advanceSetup();
     return true;
 }
+bool Game::autoPlaceShipsForCurrentPlayer() {
+    if (m_state != GameState::SetupP1 && m_state != GameState::SetupP2) return false;
 
-void Game::advanceSetup() {
-    if (m_nextShipToPlace < m_shipCount) return;
+    Player& cur = (m_state == GameState::SetupP1) ? m_p1 : m_p2;
+    bool ok = cur.placeAllShipsRandomly();
+    if (!ok) return false;
 
-    // finished placing for current setup player
-    m_nextShipToPlace = 0;
-    if (m_state == GameState::SetupP1) m_state = GameState::SetupP2;
-    else m_state = GameState::TurnP1; // after P2 setup, start P1 turn
+    m_nextShipToPlace = m_shipCount;
+    advanceSetup();
+    return true;
 }
 
 ShotResult Game::fire(Position target) {
@@ -58,10 +65,45 @@ ShotResult Game::fire(Position target) {
 
     ShotResult res = attacker.attack(defender, target);
 
+    if (attacker.isAI()) {
+        attacker.updateAIStateAfterShot(target, res);
+    }
+
     checkGameOver();
     if (!isOver()) {
-        // Only switch turn when the shot was a valid attempt (Hit/Miss/Sunk)
-        if (res.type == ShotResultType::Hit || res.type == ShotResultType::Miss || res.type == ShotResultType::Sunk) {
+        if (res.type == ShotResultType::Hit ||
+            res.type == ShotResultType::Miss ||
+            res.type == ShotResultType::Sunk) {
+            advanceTurn();
+        }
+    }
+    return res;
+}
+
+bool Game::currentPlayerIsAI() const {
+    if (m_state == GameState::TurnP1) return m_p1.isAI();
+    if (m_state == GameState::TurnP2) return m_p2.isAI();
+    return false;
+}
+
+ShotResult Game::fireAITurn() {
+    ShotResult invalid{ShotResultType::Invalid, -1};
+
+    if (!currentPlayerIsAI()) return invalid;
+    if (m_state != GameState::TurnP1 && m_state != GameState::TurnP2) return invalid;
+
+    Player& attacker = (m_state == GameState::TurnP1) ? m_p1 : m_p2;
+    Player& defender = (m_state == GameState::TurnP1) ? m_p2 : m_p1;
+
+    Position target = attacker.chooseAITarget(defender);
+    ShotResult res = attacker.attack(defender, target);
+    attacker.updateAIStateAfterShot(target, res);
+
+    checkGameOver();
+    if (!isOver()) {
+        if (res.type == ShotResultType::Hit ||
+            res.type == ShotResultType::Miss ||
+            res.type == ShotResultType::Sunk) {
             advanceTurn();
         }
     }
@@ -73,9 +115,25 @@ void Game::advanceTurn() {
     else if (m_state == GameState::TurnP2) m_state = GameState::TurnP1;
 }
 
-void Game::checkGameOver() {
-    if (m_p1.hasLost()) { m_state = GameState::GameOver; m_winner = 1; }
-    else if (m_p2.hasLost()) { m_state = GameState::GameOver; m_winner = 0; }
+void Game::advanceSetup() {
+    if (m_nextShipToPlace < m_shipCount) return;
+
+    m_nextShipToPlace = 0;
+    if (m_state == GameState::SetupP1) m_state = GameState::SetupP2;
+    else m_state = GameState::TurnP1;
 }
 
-bool Game::isOver() const { return m_state == GameState::GameOver; }
+void Game::checkGameOver() {
+    if (m_p1.hasLost()) {
+        m_state = GameState::GameOver;
+        m_winner = 1;
+    }
+    else if (m_p2.hasLost()) {
+        m_state = GameState::GameOver;
+        m_winner = 0;
+    }
+}
+
+bool Game::isOver() const {
+    return m_state == GameState::GameOver;
+}
